88807aa775828ff824cb1aa2734767b39b994f25
erick <erick_ic@163.com>
2026-07-03 00:51:28 +0800

    更新dev.go

--- backEnd/config/dev.go
+++ backEnd/config/dev.go
@@ -6,7 +6,7 @@ package config
 var Config = config{
 	DB: DBConfig{
 		//本地连接
-		DSN: "localhost:13316",
+		DSN: "root:root@tcp(localhost:13316)/gobook?charset=utf8mb4&parseTime=True&loc=Local",
 	},
 	Redis: RedisConfig{
 		//本地连接
